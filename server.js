251d1936891838148be081a7ca6134e7e301b307
230001166 <dpplasma@gmail.com>
2019-01-23 14:26:17 -0500

    Modified server.js to add database code from Heroku.

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const express = require("express");
 const SocketServer = require("ws").Server;
 const path = require("path");
 
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, "index.html");
 
