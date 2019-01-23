e08a3f4ee61d3845831b93e2f1994c8ae20a6907
230001166 <dpplasma@gmail.com>
2019-01-23 14:34:43 -0500

    Modified server code.

--- server.js
+++ server.js
@@ -4,41 +4,45 @@ const express = require("express");
 const SocketServer = require("ws").Server;
 const path = require("path");
 
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl: true,
+  ssl: true
 });
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
   }
-  client.end();
-});
+);
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, "index.html");
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX))
+  .use((req, res) => {
+    res.sendFile(INDEX);
+    let err = false;
+    client.query(err, res);
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new SocketServer({ server, clientTracking: true });
 
-wss.on("connection", function connection(ws, req) {
-
-  let err = false;
-  client.query (err, res);
-});
+wss.on("connection", function connection(ws, req) {});
 
 setInterval(() => {
-  wss.clients.forEach((client) => {
+  wss.clients.forEach(client => {
     let date = { text: new Date().toTimeString() };
-    client.send(JSON.stringify (date)); console.log ("Sending message...");
+    client.send(JSON.stringify(date));
+    console.log("Sending message...");
   });
 }, 1000);
