cbeb48ee6176c5a4c49424140963d20537baf5d4
230001166 <dpplasma@gmail.com>
2019-02-11 19:31:15 -0500

    Modified server code.

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ const INDEX = path.join(__dirname, "index.html");
 const server = express()
   .use((req, res) => {
     res.sendFile(INDEX);
-    pool.query(res);
   })
-  .get('/', (req, res) => res.render('pages/index'))
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
